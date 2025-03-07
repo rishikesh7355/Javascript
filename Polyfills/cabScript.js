45921316c9b23c8130d4599a99b032cb6eb3141a
rishikesh7355 <rishiyadav280198@gmail.com>
2025-03-07 23:35:27 +0530

    apply polyfill

--- Polyfills/cabScript.js
+++ Polyfills/cabScript.js
@@ -4,21 +4,36 @@
     skills: 'React'
 
  }
- function display(exp) {
+ function dummyFunction(exp) {
     console.log(`My name is ${this.firstname} skilled in ${this.skills} with ${exp} YOE `);
  }
 
 // display.call(obj,3);
+// display.apply(obj,[3])
 
-Function.prototype.myCall = function(context={}, ...args) {
-    //base case
-    if(typeof this !== 'function'){
-        throw new Error (this + 'it is not callable')
-    }
-    context.fn = this;
-    context.fn(...args);
+// Function.prototype.myCall = function(context={}, ...args) {
+//     //base case
+//     if(typeof this !== 'function'){
+//         throw new Error (this + 'it is not callable')
+//     }
+//     context.fn = this;
+//     context.fn(...args);
 
+// }
+// console.log(this);
+
+// dummyFunction.myCall(obj,4)
+
+// ******************************** apply ********************************* // 
+
+Function.prototype.myApply = function(context={}, args=[]) {
+      if(typeof this !== 'function'){
+         throw new Error (this + 'it is not callable');
+      }
+      if(!Array.isArray(args)  ){
+         throw new TypeError("CreatedListFromArrayLike called on non-object")
+      }
+      context.fn = this;
+      context.fn(...args);
 }
-console.log(this);
-
-display.myCall(obj,4)
+dummyFunction.apply(obj,[4]);
